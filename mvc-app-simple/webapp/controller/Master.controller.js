f348d04f4062d749cb5bda057213f271d6f99aac
Evgenii Fedoseev <abap@sapville.com>
2018-02-04 09:06:41 +0300

    mvc-app-simple: add view and controller

---
+++ mvc-app-simple/webapp/controller/Master.controller.js
@@ -0,0 +1,7 @@
+/*globals sap*/
+
+sap.ui.define(['sap/ui/core/mvc/Controller'], function (Controller) {
+  'use strict';
+  return Controller.extend('sapville.sapui-study.mvc-app-simple.controller.Master', {
+  });
+});
---
+++ mvc-app-simple/webapp/view/Master.view.js
@@ -0,0 +1,54 @@
+/*globals sap*/
+
+sap.ui.jsview('sapville.sapui-study.mvc-app-simple.view.Master', {
+  getControllerName: function () {
+    return 'sapville.sapui-study.mvc-app-simple.controller.Master';
+  },
+  createContent: function (oController) {
+
+    const aColumns = [
+      new sap.m.Column({
+        header: new sap.m.Text({
+          text: 'ID'
+        })
+      }),
+      new sap.m.Column({
+        header: new sap.m.Text({
+          text: 'Name'
+        })
+      })
+    ];
+
+    const oTableHeader = new sap.m.Toolbar({
+      content: [
+        new sap.m.Title({
+          text: 'Number of Suppliers: {/CountSuppliers}'
+        })
+      ]
+    });
+
+    const oTable = new sap.m.Table({
+      columns: aColumns,
+      headerToolbar: oTableHeader
+    });
+
+    const oTemplate = new sap.m.ColumnListItem({
+      type: 'Navigation',
+      cells: [
+        new sap.m.ObjectIdentifier({
+          text: '{ID}'
+        }),
+        new sap.m.ObjectIdentifier({
+          text: '{Name}'
+        })
+      ]
+    });
+
+    oTable.bindItems('/Suppliers', oTemplate);
+
+    return new sap.m.Page({
+      title: 'Supplier Overview',
+      content: [oTable]
+    });
+  }
+});
