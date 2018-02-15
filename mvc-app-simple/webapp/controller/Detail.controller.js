642f7fb33c1ab014f7099db410e3f939c8737886
Evgeny Fedoseev <abap@sapville.com>
2018-02-15 18:42:28 +0300

    mvc-app-simple: add custom type

--- mvc-app-simple/webapp/controller/Detail.controller.js
+++ mvc-app-simple/webapp/controller/Detail.controller.js
@@ -3,11 +3,13 @@
 sap.ui.define([
   'sap/ui/core/mvc/Controller',
   'sap/ui/core/routing/History',
-  'sapville/sapui-study/mvc-app-simple/model/formatter'
-], function (Controller, History, formatter) {
+  'sapville/sapui-study/mvc-app-simple/model/formatter',
+  'sapville/sapui-study/mvc-app-simple/model/types'
+], function (Controller, History, formatter, types) {
   'use strict';
   return Controller.extend('sapville.sapui-study.mvc-app-simple.controller.Detail', {
     formatter: formatter,
+    types: types,
     onInit: function () {
       this._oRouter = sap.ui.core.UIComponent.getRouterFor(this);
       this._oRouter.getRoute('detail').attachPatternMatched(this._onDetailMatched, this);
